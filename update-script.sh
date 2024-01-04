2fe6de6edda19dd612c3172e267c546e96fb2504
Mohammad Reza Taesiri <mtaesiri@gmail.com>
2024-01-03 17:31:02 -0700

    added update script

---
+++ update-script.sh
@@ -0,0 +1,10 @@
+!#/bin/bash
+
+rm -fr  ~/.cache/huggingface/datasets/taesiri___arxiv_qa
+/opt/homebrew/Caskroom/miniforge/base/envs/torch2/bin/python update_dataset.py
+/opt/homebrew/Caskroom/miniforge/base/envs/torch2/bin/python readme-generator.py
+
+# perform git commit and push
+git add .
+git commit -m "update dataset"
+git push origin master
